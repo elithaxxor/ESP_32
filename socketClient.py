4b1d5f5ba1608cf3086c416c9a5e9ebd657a9bc3
adel <86808805+elithaxxor@users.noreply.github.com>
2025-01-19 01:56:37 -0500

    Add files via upload

---
+++ socketClient.py
@@ -0,0 +1,19 @@
+import websocket
+
+#pip install websocket-client
+
+# Connect to WebSocket server
+ws = websocket.WebSocket()
+ws.connect("ws://192.168.1.123")
+print("Connected to WebSocket server")
+
+# Ask the user for some input and transmit it
+str = input("Say something: ")
+ws.send(str)
+
+# Wait for server to respond and print it
+result = ws.recv()
+print("Received: " + result)
+
+# Gracefully close WebSocket connection
+ws.close()
