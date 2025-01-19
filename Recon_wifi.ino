5e84abe29d0da6842ddc815a37fe3b1e54e2ca65
adel <86808805+elithaxxor@users.noreply.github.com>
2025-01-19 01:06:34 -0500

    Update Recon_wifi.ino

--- Recon_wifi.ino
+++ Recon_wifi.ino
@@ -9,35 +9,57 @@
 #include <FS.h>
 #include <SPIFFS.h>
 #include <ArduinoJson.h>
+#include <ESPmDNS.h> 
+//#include <ESPAsyncWebServer.h>
+#include <WebServer.h>
 
-#define ESP_DRD_USE_SPIFFS true
-#define JSON_CONFIG_FILE "/router_results.json"
-bool shouldSaveConfig = false;
+#define ESP_DRD_USE_SPIFFS true 
+#define JSON_CONFIG_FILE "/router_results.json" // JSON FILE FOR SERIALIZATIONS/DESERIAAIN 
+bool shouldSaveConfig = false; // FLAG FOR SAVING DATA 
+WiFiManager wm;
+WebServer server(80);
+
+char cRouterMac[50];
+char cRouterIP[50];
+char csubnetmask[50];
+char cclientIP[50];
+char cdnsIP[50];
+char cssid[50];
+char cmac1[50];
+String webpage = "<!DOCTYPE html><html><head><title>Page Title</title></head><body style='background-color: #EEEEEE;'><span style='color: #003366;'><h1>Lets generate a random number</h1><p>The random number is: </p></span></body></html>";
+        // wm.resetSettings();
+        // res = wm.autoConnect(); // auto generated AP name from chipid
+        //res = wm.autoConnect("AutoConnectAP","password"); // password protected ap
 
 void setup() {
+  
+    bool forceConfig = false;      // Change to true when testing to force configuration every time we run
+    bool spiffsSetup = loadConfigFile();
     WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
-    // it is a good practice to make sure your code sets wifi mode how you want it.
-
-    // put your setup code here, to run once:
-    Serial.begin(115200);
-    
-    WiFiManager wm;
-    //WiFi wi; 
-
-    // wm.resetSettings();
     delay(500);
+    Serial.begin(115200);
+    delay(500);
+ 
+  if (!spiffsSetup) {
+    Serial.println(F("Forcing config mode as there is no saved config"));
+    forceConfig = true;
+  }
+
+
     bool res;
-    // res = wm.autoConnect(); // auto generated AP name from chipid
     res = wm.autoConnect("FreeWiFi"); // anonymous ap
-    //res = wm.autoConnect("AutoConnectAP","password"); // password protected ap
+    if (forceConfig){
 
     if(!res) {
-        Serial.println("Failed to connect");
-        // ESP.restart();
-    } 
+        delay(3000);
+        Serial.println("[-] Failed to connect");
+        ESP.restart();
+        delay(3000);
+
+    }   
     else {
         //if you get here you have connected to the WiFi    
-        Serial.println("connected...yeey :)");
+        Serial.println("[+] connected :)");
         delay(2000);
         String ssid = String(WiFi.SSID());
         String mac1 = String(WiFi.macAddress());
@@ -47,6 +69,15 @@ void setup() {
         String subnetmask = String(WiFi.subnetMask());
         String clientIP = String(WiFi.localIP());
         String dnsIP = String(WiFi.dnsIP());
+
+    // RouterMac = WiFi.softAPmacAddress(); // Already returns a String
+    // RouterIP = WiFi.gatewayIP().toString(); // Convert IPAddress to String
+    // subnetmask = WiFi.subnetMask().toString(); // Convert IPAddress to String
+    // clientIP = WiFi.localIP().toString(); // Convert IPAddress to String
+    // dnsIP = WiFi.dnsIP().toString(); // Convert IPAddress to String
+    // ssid = WiFi.SSID(); // Already returns a String
+    // mac1 = WiFi.macAddress(); // Already returns a String
+
 
         delay(2000);
 
@@ -59,9 +90,31 @@ void setup() {
           String(WiFi.SSID()),
           String(WiFi.macAddress())
       );
+
+
+        delay(2000);
+        server.on("/", []() {
+          server.send(200, "text\html", webpage);
+        });
+        server.begin(); 
+
+
+        delay(2000);
+      saveConfig(
+          String(WiFi.softAPmacAddress()),
+          String(WiFi.gatewayIP().toString()),
+          String(WiFi.subnetMask().toString()),
+          String(WiFi.localIP().toString()),
+          String(WiFi.dnsIP().toString()),
+          String(WiFi.SSID()),
+          String(WiFi.macAddress())
+      );
+      
         // Serial.println(ssidnsIPd); 
         //printARPTable();
     }
+  }
+}
     
 
 void processWiFiInfo(String RouterMac, String RouterIP, String subnetmask, String clientIP, String dnsIP, String ssid, String mac1) {
@@ -78,8 +131,15 @@ void processWiFiInfo(String RouterMac, String RouterIP, String subnetmask, Strin
 }
 
 
+// void notFound(AsyncWebServerRequest *request){
+//   request->send(404, "text/plain", "not found");
+// }
+
+
+// ---- SERIALIZES JSON (SEND FILE) -------- ///
 void saveConfig(String RouterMac, String RouterIP, String subnetmask, String clientIP, String dnsIP, String ssid, String mac1) {
-  StaticJsonDocument<512> json; // creates json 
+  
+    StaticJsonDocument<512> json; // creates json 
     json["RouterMac"] = RouterMac;
     json["RouterIP"] = RouterIP;
     json["subnetmask"] = subnetmask;
@@ -88,14 +148,116 @@ void saveConfig(String RouterMac, String RouterIP, String subnetmask, String cli
     json["ssid"] = ssid;
     json["mac1"] = mac1;
 
-    
+    File configFile = SPIFFS.open(JSON_CONFIG_FILE, "w");
+  
+    if (!configFile)    // Error, file did not open
+    {
+      Serial.println("[-] failed to open config file for writing");
+    }
+  
+    // Serialize JSON data to write to file
+    serializeJsonPretty(json, Serial);
+    if (serializeJson(json, configFile) == 0)
+    {
+      // Error writing file
+      Serial.println(F("Failed to write to file"));
+    }
+
+    // Close file
+    configFile.close();
 }
 
-}
 
+// ------ LOADS JSON AND DESERAIALIZES (READ FILE) ------ //
+bool loadConfigFile()
+// Load existing configuration file
+{
+  // Uncomment if we need to format filesystem
+  // SPIFFS.format();
+ 
+  // Read configuration from FS json
+  Serial.println("Mounting File System...");
+ 
+  // May need to make it begin(true) first time you are using SPIFFS
+  if (SPIFFS.begin(false) || SPIFFS.begin(true))
+  {
+    Serial.println("mounted file system");
+    if (SPIFFS.exists(JSON_CONFIG_FILE))
+    {
+      // The file exists, reading and loading
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open(JSON_CONFIG_FILE, "r");
+      if (configFile)
+      {
+        Serial.println("Opened configuration file");
+        StaticJsonDocument<512> json;
+        DeserializationError error = deserializeJson(json, configFile);
+        serializeJsonPretty(json, Serial);
+        if (!error)
+        {
+          Serial.println("Parsing JSON");
+
+    // strncpy(RouterMac, json["RouterMac"].as<String>().c_str(), siz(RouterMac) - 1);
+    // strncpy(RouterIP, json["RouterIP"].as<String>().c_str(), sizeof(RouterIP) - 1);
+    // strncpy(subnetmask, json["subnetmask"].as<String>().c_str(), sizeof(subnetmask) - 1);
+    // strncpy(clientIP, json["clientIP"].as<String>().c_str(), sizeof(clientIP) - 1);
+    // strncpy(dnsIP, json["dnsIP"].as<String>().c_str(), sizeof(dnsIP) - 1);
+    // strncpy(ssid, json["ssid"].as<String>().c_str(), sizeof(ssid) - 1);
+    // strncpy(mac1, json["mac1"].as<String>().c_str(), sizeof(mac1) - 1);
+
+
+          return true;
+        }
+        else
+        {
+          // Error loading JSON data
+          Serial.println("Failed to load json config");
+        }
+      }
+    }
+  }
+  else
+  {
+    // Error mounting file system
+    Serial.println("Failed to mount FS");
+  }
+ 
+  return false;
+}
+ 
+
+void saveConfigCallback()
+// Callback notifying us of the need to save configuration
+{
+  Serial.println("[!] Should save config");
+  shouldSaveConfig = true;
+}
+ 
+void configModeCallback(WiFiManager *myWiFiManager)
+// Called when config mode launched
+{
+  Serial.println("Entered Configuration Mode");
+ 
+  Serial.print("[+] Config SSID: ");
+  Serial.println(myWiFiManager->getConfigPortalSSID());
+ 
+  Serial.print("[+] Config IP Address: ");
+  Serial.println(WiFi.softAPIP());
+}
 void loop() {
   delay(60000); // Update every 30 seconds
   //printARPTable();
+          processWiFiInfo(
+          String(WiFi.softAPmacAddress()),
+          String(WiFi.gatewayIP().toString()),
+          String(WiFi.subnetMask().toString()),
+          String(WiFi.localIP().toString()),
+          String(WiFi.dnsIP().toString()),
+          String(WiFi.SSID()),
+          String(WiFi.macAddress())
+      );
+
+      server.handleClient();
   
 }
 
